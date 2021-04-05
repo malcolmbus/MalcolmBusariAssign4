ff58ba7560a394a40a7e827ab72e3e49e8c2a389
Malcolm Busari <busarim@shernet.sheridancollege.ca>
2021-04-05 18:17:26 -0400

    Init commit

---
+++ MalcolmAssign44/src/main/java/malcolm/busari/s991523264/MalcolmActivity.java
@@ -0,0 +1,19 @@
+package malcolm.busari.s991523264;
+/**
+ * Name: Malcolm Busari
+ * Student No: 991523264
+ * Section: 1211_34780
+ */
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MalcolmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
