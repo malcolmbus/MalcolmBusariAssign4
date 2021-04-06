3ff4e72dd488219e8512ea48457516ea3b49f563
Malcolm Busari <busarim@shernet.sheridancollege.ca>
2021-04-06 18:45:22 -0400

    Set up the navigation drawer and created fragments for each navigation menu item. Updated app icon.

--- MalcolmAssign44/src/main/java/malcolm/busari/s991523264/MalcolmActivity.java
+++ MalcolmAssign44/src/main/java/malcolm/busari/s991523264/MalcolmActivity.java
@@ -5,15 +5,123 @@ package malcolm.busari.s991523264;
  * Section: 1211_34780
  */
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MalcolmActivity extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadFragment(new HomeFrag());
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        drawerLayout = findViewById(R.id.malcolmDrawerLayout);
+
+        NavigationView navigationView = findViewById(R.id.malcolmNavigationView);
+        navigationView.setNavigationItemSelectedListener(
+                menuItem -> {
+                    // set item as selected to persist highlight
+                    menuItem.setChecked(true);
+                    // close drawer when item is tapped
+                    drawerLayout.closeDrawers();
+
+                    // Add code here to update the UI based on the item selected
+                    // For example, swap UI fragments here
+                    //display in short period of time
+                    Toast.makeText(getApplicationContext(), menuItem.getTitle(),
+                            Toast.LENGTH_LONG).show();
+
+                    // Launch the corresponding fragment
+                    Fragment fragment = null;
+                    Class fragmentClass = null;
+                    switch (menuItem.getItemId()) {
+                        // display ad
+                        case R.id.nav_download:
+                            fragmentClass = DownloadFrag.class;
+                            break;
+                        case R.id.nav_webservice:
+                            fragmentClass = WebServiceFrag.class;
+                            break;
+                        default:
+                            fragmentClass = HomeFrag.class;
+                    }
+
+                    try {
+                        fragment = (Fragment) fragmentClass.newInstance();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    // Insert the fragment by replacing any existing fragment
+                    loadFragment(fragment);
+
+
+                    return true;
+                });
+
+        drawerLayout.addDrawerListener(
+                new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                        // Respond when the drawer's position changes
+                    }
+
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        // Respond when the drawer is opened
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        // Respond when the drawer is closed
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        // Respond when the drawer motion state changes
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        drawerLayout.openDrawer(GravityCompat.START);
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.malcolmFrameLayout, fragment);
+        transaction.commit();
     }
 }
