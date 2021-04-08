8ac950349ca8642dcf093326245801292351ecee
Malcolm Busari <busarim@shernet.sheridancollege.ca>
2021-04-07 23:22:14 -0400

    Changed background color of DownloadFrag.java. Added a textview, a spinner with different image options, an imageview to display the downloaded image and a download button.

--- MalcolmAssign44/src/main/java/malcolm/busari/s991523264/DownloadFrag.java
+++ MalcolmAssign44/src/main/java/malcolm/busari/s991523264/DownloadFrag.java
@@ -5,6 +5,10 @@ package malcolm.busari.s991523264;
  * Section: 1211_34780
  */
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,6 +16,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +33,13 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class DownloadFrag extends Fragment {
+
+    private static final String[] paths = {"Flower", "Nature", "Sky"};
+    URL ImageUrl = null;
+    InputStream is = null;
+    Bitmap bmImg = null;
+    ImageView imageView= null;
+    ProgressDialog p;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -63,7 +84,86 @@ public class DownloadFrag extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_download, container, false);
+        View view = inflater.inflate(R.layout.fragment_download, container, false);
+
+        Spinner spinner = view.findViewById(R.id.malcolmPhotoSpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, paths);
+        spinner.setAdapter(adapter);
+        imageView = view.findViewById(R.id.malcolmIV);
+        Button downloadBtn = view.findViewById(R.id.malcolmDownloadBtn);
+        downloadBtn.setOnClickListener(v -> spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view1, int position, long id) {
+
+                int pos = parent.getSelectedItemPosition();
+                if (pos == 0) {
+                    AsyncTaskExample asyncTask=new AsyncTaskExample();
+                    asyncTask.execute("https://upload.wikimedia.org/wikipedia/commons/5/5a/Flower_11.jpg");
+                }
+                else if(pos == 1){
+                    AsyncTaskExample asyncTask=new AsyncTaskExample();
+                    asyncTask.execute("https://upload.wikimedia.org/wikipedia/commons/b/b3/Nature_.jpg");
+                }
+                else if(pos == 2){
+                    AsyncTaskExample asyncTask=new AsyncTaskExample();
+                    asyncTask.execute("https://upload.wikimedia.org/wikipedia/commons/d/d7/Sky.jpg");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        }));
+
+        return view;
+    }
+
+    private class AsyncTaskExample extends AsyncTask<String, String, Bitmap> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            p=new ProgressDialog(getContext());
+            p.setMessage("Please wait...It is downloading");
+            p.setIndeterminate(false);
+            p.setCancelable(false);
+            p.show();
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            try {
+
+                ImageUrl = new URL(strings[0]);
+                HttpURLConnection conn = (HttpURLConnection) ImageUrl
+                        .openConnection();
+                conn.setDoInput(true);
+                conn.connect();
+                is = conn.getInputStream();
+
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inPreferredConfig = Bitmap.Config.RGB_565;
+                bmImg = BitmapFactory.decodeStream(is, null, options);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return bmImg;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            if(imageView!=null) {
+                p.hide();
+                imageView.setImageBitmap(bitmap);
+            }else {
+                p.show();
+            }
+        }
     }
 }
